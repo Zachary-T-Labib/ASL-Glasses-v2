d5196ff2b4f12676279e6313f711393c95fe2ab2
Zachary-T-Labib <67299732+Zachary-T-Labib@users.noreply.github.com>
2025-09-30 14:40:03 -0400

    added comments and created image utility file

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import mediapipe as mp
+import numpy as np
+import cv2
+import image_utilities as imageUtil
 from mediapipe import solutions
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
-import numpy as np
-import cv2
-# from google.colab.patches import cv2_imshow
 from PIL import Image
 
+# Path to mediapipe hand landmarker model
 model_path = './hand_landmarker.task'
 
 # Test image
 mp_image = mp.Image.create_from_file('./a16.jpg')
 
+# Set up options/settomgs for hand landmarker
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
@@ -28,53 +30,12 @@ with HandLandmarker.create_from_options(options) as landmarker:
     print("placeholder") 
     hand_landmarker_result = landmarker.detect(mp_image)
 
+# Print raw data from landmarker result
 print(hand_landmarker_result)
 
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
-
-    # Draw the hand landmarks.
-    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    hand_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      hand_landmarks_proto,
-      solutions.hands.HAND_CONNECTIONS,
-      solutions.drawing_styles.get_default_hand_landmarks_style(),
-      solutions.drawing_styles.get_default_hand_connections_style())
-
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
-
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-  return annotated_image
-
-
-# STEP 5: Process the classification result. In this case, visualize it.
+# Prcoessing the classigication result and displaying the annotated image
 image_reformated = cv2.cvtColor(mp_image.numpy_view(), cv2.COLOR_RGB2BGR)
-annotated_image = draw_landmarks_on_image(image_reformated, hand_landmarker_result)
+annotated_image = imageUtil.draw_landmarks_on_image(image_reformated, hand_landmarker_result)
 
 annotated_pil = Image.fromarray(cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB))
 annotated_pil.show()
